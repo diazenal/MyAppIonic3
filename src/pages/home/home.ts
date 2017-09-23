0ea9b18f09ba5e66dc7f4844411285ed51e229da
diazenal <surasak.lmt@gmail.com>
2017-09-23 23:48:20 +0700

    test comment

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  age: number;
+  sex: string = 'ชาย';
+  groups: Array<{ id: number, name: string }>=[];
+  groups2: Array<any> = [1, 2, 5, '5'];
 
+  constructor(public navCtrl: NavController) {
+    this.age = 20;
+
+    this.groups.push({ id: 1, name: 'John Doe' });
+    this.groups.push({ id: 2, name: 'Steve Job' });
   }
 
 }
