c1af8a82b8a570a5b1bb8749c06061e27c156db6
diazenal <surasak.lmt@gmail.com>
2017-09-25 21:01:00 +0700

    แสดงวิธีใช้งาน Provider และการใช้ LoadingController

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,35 +1,52 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the MainPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 import { MapPage } from './../map/map';
+
+import { UserProvider } from './../../providers/user/user';
 
 @IonicPage()
 @Component({
   selector: 'page-main',
   templateUrl: 'main.html',
+  providers:[]//ถ้าต้องการใช้แบบ Individal provider ก็ให้กำหนด
 })
 export class MainPage {
 
   users: Array<{ name: string, email: string }> = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.users.push({ name: 'John Doe', email: 'john@gmail.com' });
-    this.users.push({ name: 'Steve Job', email: 'steve@gmail.com' });
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public userProvider: UserProvider,
+    public loadingCtrl: LoadingController
+  ) {
+
+    //his.users.push({ name: 'John Doe', email: 'john@gmail.com' });
+    //this.users.push({ name: 'Steve Job', email: 'steve@gmail.com' });
   }
 
   goDetail(_user) {
     this.navCtrl.push(MapPage, { user: _user, users: this.users });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MainPage');
+  ionViewWillEnter() {
+
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait ...',
+      spinner: 'dots'
+    })
+    loading.present();
+
+    this.userProvider.getUsers()
+      .then((data: any) => {
+        this.users = data;
+
+        loading.dismiss();
+      }, (error) => {
+        loading.dismiss();
+
+      });
   }
 
 }
