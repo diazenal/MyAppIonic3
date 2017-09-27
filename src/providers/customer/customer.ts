965ee80c5fd3c9e3a9e577afa11cb53d774653b4
diazenal <surasak.lmt@gmail.com>
2017-09-27 23:33:15 +0700

    สร้าง getGroups(token) สำหรับดึงข้อมูล group

--- src/providers/customer/customer.ts
+++ src/providers/customer/customer.ts
@@ -29,4 +29,23 @@ export class CustomerProvider {
     })
   }
 
+  getGroups(token: string) {
+    return new Promise((resolve, reject) => {
+
+      let header = new Headers({
+        'Content-Type': 'application/json',
+        'x-access-token': token
+      });
+      let options = new RequestOptions({ headers: header });
+
+      this.http.get(`${this.url}/customers/groups`, options)
+        .map(res => res.json())
+        .subscribe(data => {
+          resolve(data); //ถ้า success ก็คืน data ออกไป
+        }, error => {
+          reject(error);// ถ้ามี error ก็ reject
+        });//subscribe() คือการสั่งให้ไปดึงข้อมูลมาแล้วค่อยเอา res ไปทำ map() json ที่คือกลับมากคือ data
+    })
+  }
+
 }
