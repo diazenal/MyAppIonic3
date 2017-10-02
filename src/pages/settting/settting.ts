bf945c69f55113986e8208b402a592311ad03a86
diazenal <surasak.lmt@gmail.com>
2017-10-03 01:44:32 +0700

    ไฟล์ ที่ได้จาก console firebase 1.create app on firebase 2.go to Project Settings แล้วกดปุ่ม ADD APP เลือก Add Firebase to your Android app 3.ตั้งชื่อ package name เป็นชื่อเดียวกับ widget id ของ config.xml

--- src/pages/settting/settting.ts
+++ src/pages/settting/settting.ts
@@ -1,25 +1,68 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SetttingPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+
 
 @IonicPage()
 @Component({
   selector: 'page-settting',
   templateUrl: 'settting.html',
+  providers:[Push]
 })
 export class SetttingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  accept: boolean;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private push: Push) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SetttingPage');
+  }
+
+  changeToggle() {
+    console.log(this.accept);
+    if (this.accept) {
+      // register device
+
+      // to initialize push notifications
+
+      const options: PushOptions = {
+        android: {},
+        ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'false'
+        },
+        windows: {},
+        // browser: {
+        //   pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+        // }
+      };
+
+      const pushObject: PushObject = this.push.init(options);
+
+      pushObject.on('registration')
+        .subscribe((registration: any) => {
+          console.log('Device registered', registration)
+          let deviceToken = registration.registrationId;
+
+        });
+
+      pushObject.on('notification')
+        .subscribe((notification: any) => {
+          console.log('Received a notification', notification)
+        });
+
+      pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+
+    } else {
+
+    }
   }
 
 }
